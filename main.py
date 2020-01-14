84335fe8a4cec45e4879ad49598fd50b5ff982af
reinisdreska <reinisdreska@gmail.com>
2020-01-14 09:58:21 +0000

    some

--- main.py
+++ main.py
@@ -17,4 +17,4 @@ def getAbout():
 def contact():
   return render_template('contact.html', phone = 87320514)
 
-app.run(host='0.0.0.0',port=8020)
+app.run(host='0.0.0.0', threaded=True, port=5000, debug=True)
