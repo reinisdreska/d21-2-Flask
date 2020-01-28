df3c555b6bc737f22f2d0f44b08200eede75b1ba
[Students] Reinis Dreska <81DRDreska@rvt.lv>
2020-01-28 12:25:28 +0200

    add post

--- main.py
+++ main.py
@@ -21,5 +21,9 @@ def contact():
 def params():
   return request.args
 
+@app.route('/post_req',methods=['POST'])
+def post_req():
+  return request.args
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', threaded=True, port='5000', debug=True)
