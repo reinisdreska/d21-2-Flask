f89cac3f76f995610aec006f6017d6895dda8c7b
[Students] Reinis Dreska <81DRDreska@rvt.lv>
2020-02-03 15:30:36 +0200

    teksts

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from file_proc import read_file, write_file
 
 app = Flask(__name__)
 
@@ -24,6 +25,28 @@ def params():
 @app.route('/post_req',methods=['POST'])
 def post_req():
   return request.args
+
+@app.route('/read_file')
+def read_from_file():
+  content = read_file()
+  return content
+
+@app.route('/write_file',methods=['POST'])
+def write_to_file():
+  return request.content_type
+  contentJSON=request.get_json()
+  write_file(contentJSON['data'])
+  return f"Add line {contentJSON['data']} to file."
+
+
+@app.route('/file',methods=['GET','POST'])
+def filework():
+  if request.method=='GET':
+    return read_from_file
+  elif request.method=='POST':
+    return write_to_file
+  else:
+    return f"Method is {request.method}"
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', threaded=True, port='5000', debug=True)
