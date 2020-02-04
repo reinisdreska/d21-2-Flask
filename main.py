8551b1abcda5afdd16314bdad04fb2296c95500d
[Students] Reinis Dreska <81DRDreska@rvt.lv>
2020-02-04 12:05:09 +0200

    json

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, json, jsonify
 from file_proc import read_file, write_file
 
 app = Flask(__name__)
@@ -49,6 +49,14 @@ def filework():
     return write_to_file
   else:
     return f"Method is {request.method} not supported "
+
+@app.route('/json')
+def json_get():
+  list=[]
+  list.append('value1')
+  list.append('value2')
+  list.append('value3')
+  return jsonify({'data':list})
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', threaded=True, port='5000', debug=True)
